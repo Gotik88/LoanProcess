134fb14ba1a8e47acc746ac0da89e320fdd29438
dmytro romanii <dmytro.romanii@gmail.com>
2014-04-15 21:40:30 +0300

    sss

---
+++ LoanProcess.Infrastructure/Caching/ICacheStorage.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace LoanProcess.Infrastructure.Caching
+{
+    using System;
+
+    public interface ICacheStorage : IDisposable
+    {
+        void Remove(string key);
+
+        void Store(CacheItem cacheItem);
+
+        T Retrieve<T>(string key);
+
+        ////Hashtable Load();
+
+        void Flush();
+    }
+}
